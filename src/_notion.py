4e4a743bd98103c79f075b5888596c7f13d88821
Zach Lopez <zachlopez9@gmail.com>
2021-12-16 15:35:33 -0600

    refactored again, optimized for testing

--- src/_notion.py
+++ src/_notion.py
@@ -1,4 +1,4 @@
-from base_utils.base_api import API
+from src.base_utils.base_api import API
 from requests import Response
 
 
@@ -9,10 +9,3 @@ class Notion(API):
 
     def get_page(self, page_id: str) -> Response:
         return self._get(f"/v1/pages/{page_id}")
-
-
-if __name__ == "__main__":
-    notion = Notion()
-    r = notion.get_page("96a46207e9b945ddbc5619c8f5f0d6d6")
-    r.raise_for_status()
-    print(r.json())
