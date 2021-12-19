acb6c1933221cbbb1a468bd7d6a6a36b5ee04abe
Zach Lopez <zachlopez9@gmail.com>
2021-12-19 13:08:42 -0600

    refactored according to recommended package structure

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,12 +1,13 @@
-from src.notion.models import Page, Block, FlashCard
+from src.hna.notion.models import Page, Block, FlashCard
 import pytest
 
 
 def test_page(get_json):
     dct = get_json("page")
     page = Page.create(dct)
-    assert Page.title == ""
-    assert Page._id == ""
+    assert page.title == "Introduction to Azure Fundamentals"
+    assert isinstance(page._id, str)
+    assert page.parent_page_id == "4d7ec42e-a2c6-46a7-b3ed-f5137cdeb3e5"
 
 
 def test_block(get_json):
